dfbe1d7f91f06ad473d9532556a8c77c3f98f0b6
MosqitoTorpedo <mosqitotorpedo@gmail.com>
2023-12-18 15:48:43 -0600

    get the equation and split string into seperate variables

--- src/main/java/tk/mosqitotorpedo/Main.java
+++ src/main/java/tk/mosqitotorpedo/Main.java
@@ -3,5 +3,21 @@ package tk.mosqitotorpedo;
 public class Main {
     public static void main(String[] args) {
 
+        if (args.length < 1) {
+            System.out.println("Please enter the equation");
+            System.exit(0);
+        }
+
+        String[] equation = args[0].split(" ");
+        int term1 = Integer.parseInt(equation[0]);
+        int term2 = Integer.parseInt(equation[2]);
+        String operation = equation[1];
+
+
+        System.out.println(term1 + term2 + operation);
+
+
+
+
     }
 }
