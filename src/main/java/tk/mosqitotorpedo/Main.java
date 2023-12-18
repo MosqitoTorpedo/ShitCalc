c9fa1825e3c7cd1c0c53b24cc7b85e54471532f9
MosqitoTorpedo <mosqitotorpedo@gmail.com>
2023-12-18 15:57:38 -0600

    switched branch

--- src/main/java/tk/mosqitotorpedo/Main.java
+++ src/main/java/tk/mosqitotorpedo/Main.java
@@ -1,14 +1,17 @@
 package tk.mosqitotorpedo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length < 1) {
-            System.out.println("Please enter the equation");
-            System.exit(0);
-        }
 
-        String[] equation = args[0].split(" ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter equation: ");
+        String userInput = scanner.nextLine();
+
+        String[] equation = userInput.split(" ");
         int term1 = Integer.parseInt(equation[0]);
         int term2 = Integer.parseInt(equation[2]);
         String operation = equation[1];
